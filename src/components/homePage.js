71711c593aa70a6a7b3ff7dc09e718cd21e09a2e
TravisLloyd <jackson231@Traviss-iMac.local>
2018-10-10 22:44:39 -0700

    basic commit

---
+++ src/components/homePage.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const react = require('react');
+
+const home = React.createClass({
+    render: () => {
+        return (
+            <div className="jumbotron">
+                <h1>Pluralsight Administration</h1>
+                <p>React, React Router, and flux</p>
+            </div>
+        )
+    }
+})
---
+++ src/main.js
@@ -0,0 +1,6 @@
+$ = jQuery = require('jquery');
+
+
+var App = console.log('Browserify Running');
+
+modules.exports = App;
